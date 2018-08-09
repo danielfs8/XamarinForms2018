507fd7fbffc4767e1760bbf4da3aa3d827328c0d
Daniel SantAnna <danielfs8@gmail.com>
2018-08-08 21:38:53 -0300

    Atualização do APP

---
+++ Seção 09/App1_Cell/App1_Cell/App1_Cell/Modelo/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1_Cell.Modelo
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Cargo { get; set; }
+        public string Image { get; set; }
+
+
+    }
+}
---
+++ Seção 09/App1_Cell/App1_Cell/App1_Cell/Pagina/TextCellPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1_Cell.Pagina;
+using App1_Cell.Modelo;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Cell.Pagina
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TextCellPage : ContentPage
+	{
+		public TextCellPage ()
+		{
+			InitializeComponent ();
+
+            List<Funcionario> Lista = new List<Funcionario>();
+            Lista.Add(new Funcionario() { Nome = "José", Cargo = "Gerente"});
+            Lista.Add(new Funcionario() { Nome = "Tassi", Cargo = "Admin" });
+            Lista.Add(new Funcionario() { Nome = "Daniel", Cargo = "Developer" });
+            Lista.Add(new Funcionario() { Nome = "Elaine", Cargo = "Gestora" });
+        }
+	}
+}
