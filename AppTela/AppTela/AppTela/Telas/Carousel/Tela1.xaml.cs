ed9df33b839e0a4785b04a8a2257214138ad1fef
Daniel SantAnna <danielfs8@gmail.com>
2019-05-30 14:55:24 -0300

    Estudo de Paginas Atualizaçao.

---
+++ AppTela/AppTela/AppTela/Telas/Carousel/Tela1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTela.Telas.Tipos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tela1 : ContentPage
+    {
+        public Tela1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppTela/AppTela/AppTela/Telas/Carousel/Tela3.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTela.Telas.Tipos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tela3 : ContentPage
+    {
+        public Tela3()
+        {
+            InitializeComponent();
+        }
+
+        private void MudarPagina(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new NavigationPage(new Navigation.Pagina1()) {BarBackgroundColor = Color.Azure};
+        }
+    }
+}
