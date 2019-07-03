6984534f6d2615b6abbc9e0a3e74a0372781f60d
Daniel SantAnna <danielfs8@gmail.com>
2019-07-02 22:04:06 -0300

    Inicio do AppTarefas

---
+++ App02_Xamarin/RedeSocialEmpresa/RedeSocialEmpresa/RedeSocialEmpresa/Master/Menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RedeSocialEmpresa.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoPaginaPerfil1(object sender, EventArgs args)
+        {
+           Detail = new RedeSocialEmpresa.Paginas.Perfil1();
+            IsPresented = false;
+        }
+
+        private void GoPaginaPerfil2(object sender, EventArgs args)
+        {
+            Detail = new RedeSocialEmpresa.Paginas.Perfil2();
+            IsPresented = false;
+        }
+
+    }
+}
---
+++ ControlesXF/ControlesXF/ControlesXF/Menu/Master.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControlesXF.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+        public void GoActivityIndicatoPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ActivityIndicatoPage();
+            IsPresented = false;
+        }
+        
+        public void GoProgressBarPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ProgressBarPage();
+            IsPresented = false;
+        }
+    }
+}
