8f24dcbb506ab64c74584815893124efb9243b13
Daniel SantAnna <danielfs8@gmail.com>
2019-07-17 21:13:09 -0300

    App_ListaEstados

---
+++ Secao09/Apps_ListaBrasil/Apps_ListaBrasil/Apps_ListaBrasil/Estados.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Apps_ListaBrasil.Modelo;
+
+namespace Apps_ListaBrasil
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Estados : ContentPage
+	{
+		public Estados ()
+		{
+			InitializeComponent ();
+            ListaDeEstados.ItemsSource = Servico.Servico.GetEstados();
+		}
+
+        private void SelecaoEstadosAction(object sender, SelectedItemChangedEventArgs args)
+        {
+            Estado estado =(Estado)args.SelectedItem;
+
+            Navigation.PushAsync(new Municipios(estado));
+        }
+    }
+}
---
+++ Secao09/Apps_ListaBrasil/Apps_ListaBrasil/Apps_ListaBrasil/Modelo/Municipio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apps_ListaBrasil.Modelo
+{
+    public class Municipio
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+    }
+
+   
+}
