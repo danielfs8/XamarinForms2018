41ba887c246a0b53407ec8da7ffded60d876c155
Daniel SantAnna <danielfs8@gmail.com>
2019-08-25 02:19:23 -0300

    Subindo App com Consulta a um service SOAP

---
+++ Secao13/App2_SOAPClient/App2_SOAPClient/App2_SOAPClient.iOS/Service/ServiceSOAP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(App2_SOAPClient.iOS.Service.ServiceSOAP))]
+namespace App2_SOAPClient.iOS.Service
+{
+    public class ServiceSOAP : IServiceSOAP
+    {
+        public string Somar(int num1, int num2)
+        {
+            var Serv = new com.dneonline.www.Calculator();
+            var resposta = Serv.Add(num1, num2);
+
+            return "Serviço IOS: " + resposta;
+        }
+    }
+}
